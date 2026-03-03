1ef044b0b73a6b49d94b0798d11af167e0d87ecd
123cobloc <61994791+123cobloc@users.noreply.github.com>
2026-03-03 15:47:20 +0100

    Change run-at timing and update style application logic

--- uirework.user.js
+++ uirework.user.js
@@ -7,7 +7,7 @@
 // @match        https://www.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
 // @grant        none
-// @run-at       document-start
+// @run-at       document-idle 
 // @supportURL   https://github.com/123cobloc/geoguessr-ui-rework/issues
 // @downloadURL  https://github.com/123cobloc/geoguessr-ui-rework/raw/main/uirework.user.js
 // @updateURL    https://github.com/123cobloc/geoguessr-ui-rework/raw/main/uirework.user.js
@@ -24,24 +24,24 @@
         div[class*="spectate-sidebar_root"] {
             display: none !important;
         }
-
+        
         div[class*="spectate-header_root"] {
             height: ${value}svh !important;
             width: ${value * 2.25}svw !important;
             margin: 0 auto !important;
         }
-
+        
         div[class*="spectate_layout_root"] {
             padding: calc(10svw / 6) !important;
         }
-
+        
         div[class*="settings_settingsButtonContainer"] {
             top: 2.5svh !important;
             right: 1.5svw !important;
             bottom: auto !important;
             left: auto !important;
         }
-
+        
         div[class*="spectate-map_buttonWrapper"] {
             bottom: calc(10svw / -6.3) !important;
         }
@@ -51,31 +51,31 @@
         const isSpectatePage = /\/duels\/.*\/spectate/.test(window.location.href);
         let styleElement = document.getElementById(styleId);
 
-        if (isSpectatePage && !styleElement) {
-            styleElement = document.createElement("style");
-            styleElement.id = styleId;
-            styleElement.textContent = newCss;
-            document.documentElement.appendChild(styleElement);
-        } else if (!isSpectatePage && styleElement) {
+        if (isSpectatePage) {
+            if (!styleElement) {
+                styleElement = document.createElement("style");
+                styleElement.id = styleId;
+                styleElement.textContent = newCss;
+                (document.head || document.documentElement).appendChild(styleElement);
+            }
+        } else if (styleElement) {
             styleElement.remove();
         }
     }
 
-    const originalPushState = history.pushState;
-    const originalReplaceState = history.replaceState;
+    // --- Gestione della navigazione SPA ---
+    
+    // Osservatore per i cambiamenti nell'URL (metodo più affidabile)
+    let lastUrl = location.href;
+    const observer = new MutationObserver(() => {
+        if (location.href !== lastUrl) {
+            lastUrl = location.href;
+            applyStyle();
+        }
+    });
 
-    history.pushState = function() {
-        originalPushState.apply(this, arguments);
-        applyStyle();
-    };
+    observer.observe(document, { subtree: true, childList: true });
 
-    history.replaceState = function() {
-        originalReplaceState.apply(this, arguments);
-        applyStyle();
-    };
-
-    window.addEventListener('popstate', applyStyle);
-
+    // Eseguiamo anche all'avvio
     applyStyle();
-
 })();
