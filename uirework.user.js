c19eced8b7d396e75e7a8178d449707338ee0fd6
123cobloc <61994791+123cobloc@users.noreply.github.com>
2026-03-03 21:17:24 +0100

    v1.2

--- uirework.user.js
+++ uirework.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         GeoGuessr Spectate UI Rework
 // @namespace    123cobloc
-// @version      1.1
+// @version      1.2
 // @description  Applies custom CSS on the spectate page
 // @author       123cobloc
 // @match        https://www.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
 // @run-at       document-idle
 // @supportURL   https://github.com/123cobloc/geoguessr-ui-rework/issues
 // @downloadURL  https://github.com/123cobloc/geoguessr-ui-rework/raw/main/uirework.user.js
@@ -17,35 +19,53 @@
 (function() {
     'use strict';
 
-    const value = 23;
+    let value = GM_getValue("svh_value", 23);
     const styleId = "custom-spectate-style";
 
-    const newCss = `
-        div[class*="spectate-sidebar_root"] {
-            display: none !important;
-        }
+    function updateValue() {
+        let newValue = prompt("Set the header height.\nThe value is the percentage of screen taken by the header.\nSuggested value: 23", value);
+        if (newValue !== null && !isNaN(newValue)) {
+            value = parseFloat(newValue);
+            GM_setValue("svh_value", value);
 
-        div[class*="spectate-header_root"] {
-            height: ${value}svh !important;
-            width: ${value * 2.25}svw !important;
-            margin: 0 auto !important;
+            const styleElement = document.getElementById(styleId);
+            if (styleElement) {
+                styleElement.remove();
+                applyStyle();
+            }
         }
+    }
 
-        div[class*="spectate_layout_root"] {
-            padding: calc(10svw / 6) !important;
-        }
+    GM_registerMenuCommand("Set header height", updateValue);
 
-        div[class*="settings_settingsButtonContainer"] {
-            top: 2.5svh !important;
-            right: 1.5svw !important;
-            bottom: auto !important;
-            left: auto !important;
-        }
+    function getCss() {
+        return `
+            div[class*="spectate-sidebar_root"] {
+                display: none !important;
+            }
 
-        div[class*="spectate-map_buttonWrapper"] {
-            bottom: calc(10svw / -6.3) !important;
-        }
-    `;
+            div[class*="spectate-header_root"] {
+                height: ${value}svh !important;
+                width: ${value * 2.25}svw !important;
+                margin: 0 auto !important;
+            }
+
+            div[class*="spectate_layout_root"] {
+                padding: calc(10svw / 6) !important;
+            }
+
+            div[class*="settings_settingsButtonContainer"] {
+                top: 2.5svh !important;
+                right: 1.5svw !important;
+                bottom: auto !important;
+                left: auto !important;
+            }
+
+            div[class*="spectate-map_buttonWrapper"] {
+                bottom: calc(10svw / -6.3) !important;
+            }
+        `
+    };
 
     function applyStyle() {
         const isSpectatePage = /\/duels\/.*\/spectate/.test(window.location.href);
@@ -55,7 +75,7 @@
             if (!styleElement) {
                 styleElement = document.createElement("style");
                 styleElement.id = styleId;
-                styleElement.textContent = newCss;
+                styleElement.textContent = getCss();
                 document.head.appendChild(styleElement);
             }
         } else {
